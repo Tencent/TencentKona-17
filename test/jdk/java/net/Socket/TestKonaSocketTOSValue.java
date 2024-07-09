478e5d99a5b8bc34032aa5b841bdaa7ae22a0d54
mck156 <125785475+mck156@users.noreply.github.com>
2024-07-09 09:16:26 +0800

    Update TestKonaSocketTOSValue.java

--- test/jdk/java/net/Socket/TestKonaSocketTOSValue.java
+++ test/jdk/java/net/Socket/TestKonaSocketTOSValue.java
@@ -20,6 +20,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 /*
  * @test
  * @summary Test the property of -Dkona.socket.tos.value
@@ -33,6 +34,7 @@ import java.nio.channels.*;
 import javax.net.*;
 import javax.net.ssl.*;
 import jdk.net.Sockets;
+
 import jdk.test.lib.process.OutputAnalyzer;
 import jdk.test.lib.process.ProcessTools;
 
@@ -42,17 +44,21 @@ public class TestKonaSocketTOSValue {
         testOptions("-Dkona.socket.tos.value=96", "96");
         testOptions("-Dkona.socket.tos.value=104", "104");
         testOptions("-Dkona.socket.tos.value=0", "0");
+        
         testFailure("-Dkona.socket.tos.value=-1");
         testFailure("-Dkona.socket.tos.value=256");
+        
         testSetTraffic("-Dkona.socket.tos.value=96", "128");
         testSetTraffic("-Dkona.socket.tos.value=0", "96");
     }
+    
     static void testFailure(String arg) throws Exception {
         ProcessBuilder pb = ProcessTools.createJavaProcessBuilder(arg, Test.class.getName());
         OutputAnalyzer output = new OutputAnalyzer(pb.start());
         output.shouldContain("Invalid IP_TOS value");
         output.shouldContain("Exception");
     }
+    
     static void testSetTraffic(String arg, String traffic) throws Exception {
         ProcessBuilder pb = ProcessTools.createJavaProcessBuilder(arg, Test.class.getName(), traffic);
         OutputAnalyzer output = new OutputAnalyzer(pb.start());
@@ -68,6 +74,7 @@ public class TestKonaSocketTOSValue {
         output.shouldContain("Proxy socket: " + traffic);
         output.shouldHaveExitValue(0);
     }
+    
     static void testOptions(String arg, String tos) throws Exception {
         ProcessBuilder pb;
         if (arg != null) {
@@ -75,6 +82,7 @@ public class TestKonaSocketTOSValue {
         } else {
             pb = ProcessTools.createJavaProcessBuilder(Test.class.getName());
         }
+        
         // check the output
         OutputAnalyzer output = new OutputAnalyzer(pb.start());
         output.shouldContain("socket: " + tos);
@@ -89,6 +97,7 @@ public class TestKonaSocketTOSValue {
         output.shouldContain("Proxy socket: " + tos);
         output.shouldHaveExitValue(0);
     }
+    
     static class Test {
         public static void main(String ...args) throws Exception {
             Socket s = new Socket();
@@ -96,44 +105,52 @@ public class TestKonaSocketTOSValue {
                 s.setTrafficClass(Integer.parseInt(args[0]));
             }
             System.out.println("socket: " + s.getOption(StandardSocketOptions.IP_TOS));
+            
             ServerSocket ss = new ServerSocket();
             if (args.length > 0) {
                 ss.setOption(StandardSocketOptions.IP_TOS, Integer.parseInt(args[0]));
             }
             System.out.println("server socket: " + ss.getOption(StandardSocketOptions.IP_TOS));
+            
             SocketChannel sc = SocketChannel.open();
             if (args.length > 0) {
                 sc.socket().setOption(StandardSocketOptions.IP_TOS, Integer.parseInt(args[0]));
             }
             System.out.println("socket channel: " + sc.socket().getOption(StandardSocketOptions.IP_TOS));
+            
             DatagramSocket ds = new DatagramSocket();
             if (args.length > 0) {
                 ds.setOption(StandardSocketOptions.IP_TOS, Integer.parseInt(args[0]));
             }
             System.out.println("datagram socket: " + ds.getOption(StandardSocketOptions.IP_TOS));
+            
             DatagramChannel dc = DatagramChannel.open();
             if (args.length > 0) {
                 dc.socket().setOption(StandardSocketOptions.IP_TOS, Integer.parseInt(args[0]));
             }
             System.out.println("datagram channel: " + dc.socket().getOption(StandardSocketOptions.IP_TOS));
+            
             SocketFactory socketFactory = SocketFactory.getDefault();
             Socket fs = socketFactory.createSocket();
             if (args.length > 0) {
                 fs.setOption(StandardSocketOptions.IP_TOS, Integer.parseInt(args[0]));
             }
             System.out.println("socket fatory: " + fs.getOption(StandardSocketOptions.IP_TOS));
+            
             ServerSocketFactory serverSocketFactory = ServerSocketFactory.getDefault();
             ServerSocket sfs = serverSocketFactory.createServerSocket();
             if (args.length > 0) {
                 Sockets.setOption(sfs, StandardSocketOptions.IP_TOS, Integer.parseInt(args[0]));
             }
             System.out.println("serversocket fatory: " + sfs.getOption(StandardSocketOptions.IP_TOS));
+            
             SSLSocketFactory ssf = (SSLSocketFactory) SSLSocketFactory.getDefault();
             SSLSocket ssls = (SSLSocket) ssf.createSocket();
             if (args.length > 0) {
                 ssls.setTrafficClass(Integer.parseInt(args[0]));
             }
             System.out.println("SSL socket: " + ssls.getOption(StandardSocketOptions.IP_TOS));
+            
             SocketAddress addr = new InetSocketAddress("www.tencent.com", 9999);
             Proxy proxy = new Proxy(Proxy.Type.SOCKS, addr);
             Socket sp = new Socket(proxy);
@@ -141,6 +158,7 @@ public class TestKonaSocketTOSValue {
                 sp.setOption(StandardSocketOptions.IP_TOS, Integer.parseInt(args[0]));
             }
             System.out.println("Proxy socket: " + sp.getOption(StandardSocketOptions.IP_TOS));
+            
             MulticastSocket multicastSocket = new MulticastSocket();
             if (args.length > 0) {
                 multicastSocket.setOption(StandardSocketOptions.IP_TOS, Integer.parseInt(args[0]));
